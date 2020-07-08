67b7547e4875487cab35b32cc36c4cbfd8bd5f81
Tomasz Kociołek <kociolek.t@gmail.com>
2020-07-08 02:05:21 +0200

    updated page

--- docs-src/src/pages/index.js
+++ docs-src/src/pages/index.js
@@ -11,12 +11,31 @@ const IndexPage = () => (
     <MainLayoutSide>
       <div className="box">
         <h2 className="box__title">Download</h2>
-        <a href="https://github.com/kociolekt/duck-ninja/releases/download/0.0.2/duck-ninja.0.0.2.zip" target="_blank">for Windows</a>
+        <a href="https://github.com/kociolekt/duck-ninja/releases/download/0.0.3/duck-ninja-windows.0.0.3.zip" target="_blank">for Windows</a><br/>
+        <a href="https://github.com/kociolekt/duck-ninja/releases/download/0.0.3/duck-ninja-mac.0.0.3.zip" target="_blank">for Mac</a>
       </div>
     </MainLayoutSide>
     <MainLayoutContainer>
       <div className="change-log">
         <h2 className="change-log__title">Changelog</h2>
+        <div className="change-log__entry">
+          <h2 className="change-log__entry-title">alpha 0.0.3</h2>
+          <p className="change-log__entry-description">
+            Prepared map to be more challenging and changed companion.<br/>
+          </p>
+          <ul className="change-log__entry-list">
+            <li>
+              changed companion apprance and added animations
+            </li>
+            <li>
+              new map generator
+            </li>
+            <li>
+              
+              export for mac
+            </li>
+          </ul>
+        </div>
         <div className="change-log__entry">
           <h2 className="change-log__entry-title">alpha 0.0.2</h2>
           <p className="change-log__entry-description">
